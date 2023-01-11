f5ed2f1294d6667e160403eb3e168468957fa0fa
麋鹿先生 <alulu@miluxianshengdeMacBook-Pro.local>
2023-01-11 15:22:23 +0800

    Temp test in day 1.10.

--- tmp/main.go
+++ tmp/main.go
@@ -1,29 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"unsafe"
+)
+
 func main() {
+	var s string = "hello"
+	fmt.Println("original string:", s)
+	modifyString(&s)
+	fmt.Println(s)
 
 }
 
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+func modifyString(s *string) {
+	p := (*uintptr)(unsafe.Pointer(s))
+	var arr *[5]byte = (*[5]byte)(unsafe.Pointer(*p))
+	var len *int = (*int)(unsafe.Pointer(uintptr(unsafe.Pointer(s)) + unsafe.Sizeof((*uintptr)(nil))))
 
-func detectCycle(head *ListNode) (ans *ListNode) {
-	slow, fast := head, head
-	for fast != nil && fast.Next != nil {
-		fast = fast.Next.Next
-		slow = slow.Next
-		if slow == fast {
-			slow = head
-			for fast != nil && fast.Next != nil {
-				fast = fast.Next.Next
-				slow = slow.Next
-				if fast == slow {
-					return fast
-				}
-			}
-		}
+	for i := 0; i < (*len); i++ {
+		fmt.Printf("%p => %c\n", &((*arr)[i]), (*arr)[i])
+		p1 := &((*arr)[i])
+		v := (*p1)
+		(*p1) = v + 1
 	}
-	return
 }
